3c0b38f5279c2f0e274bb2aa4fab7f9ec8fd42c7
sklaw <271462243@qq.com>
2017-07-05 21:26:32 +0800

    new HMM train which supports variable states

---
+++ Baxter_PNP/endpose/train/training_config.py
@@ -0,0 +1,10 @@
+interested_data_fields = [
+    u'.endpoint_state.pose.position.x',
+    u'.endpoint_state.pose.position.y',
+    u'.endpoint_state.pose.position.z',
+    u'.endpoint_state.pose.orientation.x',
+    u'.endpoint_state.pose.orientation.y',
+    u'.endpoint_state.pose.orientation.z',
+    u'.endpoint_state.pose.orientation.w',
+    u'.tag'
+]
