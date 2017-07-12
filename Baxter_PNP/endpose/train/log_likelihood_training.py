c74ab7ed8463cf097d53e54dc155a37ab86370a8
sklaw <271462243@qq.com>
2017-07-12 13:13:52 +0800

    measure time cost in log likelihook plotting

--- Baxter_PNP/endpose/train/log_likelihood_training.py
+++ Baxter_PNP/endpose/train/log_likelihood_training.py
@@ -10,6 +10,7 @@ from math import (
 )
 from matplotlib import pyplot as plt
 import ipdb
+import time
 
 def make_trials_of_each_state_the_same_length(trials_group_by_folder_name):
     # may implement DTW in the future...
@@ -34,21 +35,9 @@ def make_trials_of_each_state_the_same_length(trials_group_by_folder_name):
 
     return trials_group_by_folder_name
 
-def assess_threshold_and_decide(mean_of_log_curve, std_of_log_curve, np_matrix_traj_by_time, curve_owner, state_no, figure_save_path):
+def assess_threshold_and_decide(mean_of_log_curve, std_of_log_curve, np_matrix_traj_by_time, curve_owner, state_no, figure_save_path, score_time_cost_per_point):
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
-    
-    # plot std value boundaries
-    interested_boundary = np.arange(0, 0.1, 0.01)
-    idx = 0
-    for col_no in range(std_of_log_curve.shape[1]):
-        if std_of_log_curve[0, col_no]/mean_of_log_curve[0, col_no] > interested_boundary[idx]:
-            ax.axvline(x = col_no) 
-            ax.text(col_no, 0, 'std>%s*mean'%(interested_boundary[idx],), rotation=90)
-            idx += 1
-            if idx == len(interested_boundary):
-                break;
-        
 
     # plot log curves of all trials
     for row_no in range(np_matrix_traj_by_time.shape[0]):
@@ -75,7 +64,7 @@ def assess_threshold_and_decide(mean_of_log_curve, std_of_log_curve, np_matrix_t
     while True:
         i_str = raw_input()
         if i_str == 'ok':
-            title = 'state %s use threshold with c=%s'%(state_no, c)
+            title = 'state %s use threshold with c=%s (on average use %ss to compute each log likelihood point)'%(state_no, c, score_time_cost_per_point)
             ax.set_title(title)
             if not os.path.isdir(figure_save_path+'/threshold_assessment'):
                 os.makedirs(figure_save_path+'/threshold_assessment')
@@ -117,15 +106,29 @@ def run(model_save_path,
     std_of_log = []
     threshold = []
 
+
+
+
     for state_no in range(1, state_amount+1):
+        compute_score_time_cost = 0
+        total_step_times = 0
+
+
         all_log_curves_of_this_state = []
         curve_owner = []
         for trial_name in trials_group_by_folder_name:
             curve_owner.append(trial_name)
             one_log_curve_of_this_state = [] 
+
+            start_time = time.time()
+
             for time_step in range(len(trials_group_by_folder_name[trial_name][state_no])):
                 log_probability = model_group_by_state[state_no].score(trials_group_by_folder_name[trial_name][state_no][:time_step+1])
                 one_log_curve_of_this_state.append(log_probability)
+
+            compute_score_time_cost += time.time()-start_time
+            total_step_times += len(trials_group_by_folder_name[trial_name][state_no])
+
             all_log_curves_of_this_state.append(one_log_curve_of_this_state)
 
         # use np matrix to facilitate the computation of mean curve and std 
@@ -133,7 +136,9 @@ def run(model_save_path,
         mean_of_log_curve = np_matrix_traj_by_time.mean(0)
         std_of_log_curve = np_matrix_traj_by_time.std(0)
 
-        decided_threshold_log_curve = assess_threshold_and_decide(mean_of_log_curve, std_of_log_curve, np_matrix_traj_by_time, curve_owner, state_no, figure_save_path)
+        score_time_cost_per_point = float(compute_score_time_cost)/total_step_times
+
+        decided_threshold_log_curve = assess_threshold_and_decide(mean_of_log_curve, std_of_log_curve, np_matrix_traj_by_time, curve_owner, state_no, figure_save_path, score_time_cost_per_point)
         expected_log.append(mean_of_log_curve.tolist()[0])
         threshold.append(decided_threshold_log_curve.tolist()[0])
         std_of_log.append(std_of_log_curve.tolist()[0])
