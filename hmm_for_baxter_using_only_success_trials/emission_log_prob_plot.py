8051eed049984f06e31d599b86d46a720193b502
sklaw <none>
2017-09-12 13:46:49 +0800

    plot more in emission plots

--- hmm_for_baxter_using_only_success_trials/emission_log_prob_plot.py
+++ hmm_for_baxter_using_only_success_trials/emission_log_prob_plot.py
@@ -17,6 +17,7 @@ import ipdb
 
 
 def plot_log_prob_of_all_trials(
+    gradient_traj_by_time,
     list_of_log_prob_mat,
     log_prob_owner, 
     state_no, 
@@ -28,9 +29,9 @@ def plot_log_prob_of_all_trials(
     hidden_state_amount = list_of_log_prob_mat[0].shape[1]
 
 
-    subplot_per_row = 4
+    subplot_per_row = 3
 
-    subplot_amount = trial_amount*2
+    subplot_amount = trial_amount*3
     row_amount = int(math.ceil(float(subplot_amount)/subplot_per_row))
     fig, ax_mat = plt.subplots(nrows=row_amount, ncols=subplot_per_row)
     if row_amount == 1:
@@ -39,12 +40,17 @@ def plot_log_prob_of_all_trials(
 
     ax_list = []
     for i in range(trial_amount):
-        j = 2*i
+        j = 3*i
         row_no = j/subplot_per_row
         col_no = j%subplot_per_row
         ax_list.append(ax_mat[row_no, col_no])
 
-        j = 2*i+1
+        j = 3*i+1
+        row_no = j/subplot_per_row
+        col_no = j%subplot_per_row
+        ax_list.append(ax_mat[row_no, col_no])
+
+        j = 3*i+2
         row_no = j/subplot_per_row
         col_no = j%subplot_per_row
         ax_list.append(ax_mat[row_no, col_no])
@@ -56,13 +62,15 @@ def plot_log_prob_of_all_trials(
     for trial_no in range(trial_amount):
         log_prob_mat = list_of_log_prob_mat[trial_no][:, :].transpose()
 
-        plot_idx = 2*trial_no
+        plot_idx = 3*trial_no+1
         for hstate_no in range(hidden_state_amount):
             ax_list[plot_idx].plot(log_prob_mat[hstate_no].tolist(), linestyle="solid", color=colors_for_hstate[hstate_no])
 
         trial_name = log_prob_owner[trial_no]
 
-        ax_list[plot_idx].set_title(trial_name)
+        ax_list[plot_idx].set_title('emission probabilities')
+        ax_list[plot_idx].set_xlabel('time step')
+        ax_list[plot_idx].set_ylabel('log probability')
         ymax = np.max(log_prob_mat)
         ax_list[plot_idx].set_ylim(ymin=0, ymax=ymax)
 
@@ -78,16 +86,27 @@ def plot_log_prob_of_all_trials(
         )
         import matplotlib.image as mpimg
         img=mpimg.imread(vp_triangle_img)
-        plot_idx = 2*trial_no+1
+        plot_idx = 3*trial_no
         ax_list[plot_idx].imshow(img)
-        
+        ax_list[plot_idx].set_title('growing viterbi paths')
+        ax_list[plot_idx].set_ylabel('time step')
+        ax_list[plot_idx].set_xlabel('length of viterbi path')
 
+
+        plot_idx = 3*trial_no+2
+        ax_list[plot_idx].plot(gradient_traj_by_time[trial_no].tolist()[0], linestyle="solid", color='black')
+        ax_list[plot_idx].set_title('gradient of log-likelihood')
+        ax_list[plot_idx].set_xlabel('time step')
+        ax_list[plot_idx].set_ylabel('log probability')
+        ymax = np.max(log_prob_mat)
+        
 
     fig.set_size_inches(4*subplot_per_row,4*row_amount)
 
     if not os.path.isdir(figure_save_path+'/emission_log_prob_plot'):
         os.makedirs(figure_save_path+'/emission_log_prob_plot')
     title = 'state %s emission log_prob plot'%(state_no,)
+    fig.tight_layout()
     fig.savefig(os.path.join(figure_save_path, 'emission_log_prob_plot', title+".eps"), format="eps")
     fig.savefig(os.path.join(figure_save_path, 'emission_log_prob_plot', title+".png"), format="png")
     plt.close(1)
@@ -124,6 +143,7 @@ def run(model_save_path,
 
     for state_no in model_group_by_state:
 
+        all_log_curves_of_this_state = []
         list_of_log_prob_mat = []
         log_prob_owner = []
         for trial_name in trials_group_by_folder_name:
@@ -137,8 +157,19 @@ def run(model_save_path,
 
             list_of_log_prob_mat.append(emission_log_prob_mat)
 
+            one_log_curve_of_this_state = util.fast_log_curve_calculation(
+                trials_group_by_folder_name[trial_name][state_no],
+                model_group_by_state[state_no]
+            )
+
+            all_log_curves_of_this_state.append(one_log_curve_of_this_state)
         # use np matrix to facilitate the computation of mean curve and std 
+        np_matrix_traj_by_time = np.matrix(all_log_curves_of_this_state)
+
+        gradient_traj_by_time = np_matrix_traj_by_time[:, 1:]-np_matrix_traj_by_time[:, :-1]
+
         plot_log_prob_of_all_trials(
+            gradient_traj_by_time,
             list_of_log_prob_mat, 
             log_prob_owner, 
             state_no, 
